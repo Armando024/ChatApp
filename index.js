745229828b58a42650d36ed06f32c38dd4da2496
Armando <Armando024@live.com>
2018-09-11 16:29:39 -0400

    added username functionality

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 var app = require('express')();
+var bodyparser=require('body-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+
+//app config
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/', function(req, res){
+    console.log(req.body);
+    res.redirect('/chat/'+req.body.usrname);
+    //res.sendFile(__dirname + '/chat.html');
+});
+
+app.get('/chat/*', function(req, res){
+  res.sendFile(__dirname + '/chat.html');
+});
+
 io.on('connection', function(socket){
   console.log('a user connected');
-    io.emit('User Connects');
+    //io.emit('User Connects');
+    socket.on('User Connects',function(usrname){
+        io.emit('User Connects',usrname); 
+    });
   socket.on('disconnect', function(){
     console.log('user disconnected');
     io.emit('User disconnected');
@@ -17,10 +37,13 @@ io.on('connection', function(socket){
       io.emit('chat message',msg);
 //    console.log('message: ' + msg);
   });
-
   socket.on('user typing', function(){
       io.emit('user typing');
       console.log('user typing');
+  });
+  socket.on('user NOTtyping', function(){
+      io.emit('user NOTtyping');
+      console.log('user stop typing');
   });
 });
 
